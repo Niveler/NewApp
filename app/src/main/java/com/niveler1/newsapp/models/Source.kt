e2efc52acbddd43c5293e6f5d4bd8c8067488b7b
Niveler <niveler1@gmail.com>
2022-12-22 12:46:42 +0300

    lesson-3

---
+++ app/src/main/java/com/niveler1/newsapp/models/Source.kt
@@ -0,0 +1,6 @@
+package com.niveler1.newsapp.models
+
+data class Source(
+    val id: String,
+    val name: String
+)
---
+++ app/src/main/java/com/niveler1/newsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.niveler1.newsapp.utils
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://newsapi.org"
+        const val API_KEY = "09c49dc34a774bff89babb5c09f4a18f"
+    }
+}
