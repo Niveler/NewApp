58b8dd1bace891899afb3b3300dde77eb47edc33
Niveler <niveler1@gmail.com>
2022-12-22 13:36:30 +0300

    lesson-3

--- app/src/main/java/com/niveler1/newsapp/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/niveler1/newsapp/ui/details/DetailsFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.niveler1.newsapp.R
 import com.niveler1.newsapp.databinding.FragmentDetailsBinding
 
 
